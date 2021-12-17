dd76ceee41b740b30fc730435b4e5aa5a17d0f14
fr3chtoa45t <jcronier777@gmail.com>
2021-12-16 23:47:14 -0800

    Set up express server

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
-import express from "express";
+import express from 'express';
+import bodyParser from 'body-parser';
+// import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
 
+const __dirname = path.resolve();
+
+dotenv.config();
+
+// Routers.
+// import itemRoutes from './routes/items.js';
+// import orderRoutes from './routes/orders.js';
+// import tableRoutes from './routes/tables.js';
+// import paymentIntent from './routes/paymentIntent.js';
+// import receiptRoutes from './routes/receipts.js';
+
+// Start new Express application.
+const app = express();
+
+// Middleware.
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// app.use('/items', itemRoutes);
+// app.use('/orders', orderRoutes);
+// app.use('/tables', tableRoutes);
+// app.use('/paymentintent', paymentIntent);
+// app.use('/receipts', receiptRoutes);
+
+// app.use(express.static(path.join(__dirname + '/client/build')));
+// app.use(express.static(path.join(__dirname +  '/restaurant/client/build')));
+
+// MongoDB
+// const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 3001;
+
+app.get('/', function(req, res) {
+  res.send("Hello");
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
+
+// mongoose.connect(CONNECTION_URL)
+//   .then(() => app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`)))
+//   .catch((error) => console.log(error.message));
